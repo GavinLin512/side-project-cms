2ebc734527489b71d79e52660f75e4167b79ca45
GavinLin512 <81368692+GavinLin512@users.noreply.github.com>
2025-07-13 20:54:02 +0800

    更新 Next.js 配置以忽略構建期間的 ESLint 錯誤，並將 lint 命令更改為使用 Biome 進行檢查，新增 Biome 相關依賴項。

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // 在構建時忽略 ESLint 錯誤，因為使用 Biome 來處理 ESLint 錯誤
+  eslint: { ignoreDuringBuilds: true },
 };
 
 export default nextConfig;
