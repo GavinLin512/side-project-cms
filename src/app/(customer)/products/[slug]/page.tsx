55a44e6ca061a372c51252bd7a9aaf1cbc84ffe5
GavinLin512 <81368692+GavinLin512@users.noreply.github.com>
2026-01-04 15:46:45 +0800

    style: 格式化產品詳情頁面元件的程式碼

--- src/app/(customer)/products/[slug]/page.tsx
+++ src/app/(customer)/products/[slug]/page.tsx
@@ -1,12 +1,8 @@
 // test CI
-export default function ProductPage({
-    params,
-}: {
-    params: { slug: string };
-}) {
-    return (
-        <div>
-            <h1>Product Page {params.slug}</h1>
-        </div>
-    )
+export default function ProductPage({ params }: { params: { slug: string } }) {
+  return (
+    <div>
+      <h1>Product Page {params.slug}</h1>
+    </div>
+  );
 }
